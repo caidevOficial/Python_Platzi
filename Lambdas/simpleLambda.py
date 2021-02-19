cc771e4c7d0f44638c49883339af6f574f90163f
CaidevOficial <falcone.facu@gmail.com>
2021-02-18 22:22:22 -0300

    Simple Function using Lambda

--- Lambdas/simpleLambda.py
+++ Lambdas/simpleLambda.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-def simpleLambda(s, h, e, l, L, c, o, d, E):
+def simpleLambda(s:list, h:list, e:list, l:list, L:list, c:list, o:list, d:list, E:list):
     """
     Prints a message by decoding a hexa into a char.
     """
