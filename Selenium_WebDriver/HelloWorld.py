c9b4a704789637825c8f7c9aa3e9bbb734d24992
CaidevOficial <falcone.facu@gmail.com>
2021-03-15 01:00:52 -0300

    Selenium_WebDriver

---
+++ Selenium_WebDriver/HelloWorld.py
@@ -0,0 +1,42 @@
+# Copyright (C) 2021 <FacuFalcone - CaidevOficial>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+import os
+from pyunitreport import HTMLTestRunner
+from selenium import webdriver
+
+
+
+class HelloWorld(unittest.TestCase):
+    #currentDir = os.path.dirname(os.path.realpath(__file__))
+
+    def setUp(self):
+        
+        self.driver = webdriver.Chrome(executable_path=r'chromedriver_win32/chromedriver.exe')
+        driver = self.driver
+        driver.implicitly_wait(10)
+
+    def test_hello_world(self):
+        driver = self.driver
+        driver.get('https://www.google.com.ar')
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2, testRunner=HTMLTestRunner(output='reports', report_name='hello-world-reports'))
+    
